d14813b612f3217827f1d825a2e927ffcc8a1eae
Daniel Leonardo Niko <daniel.gultom@gmail.com>
2021-10-22 15:34:11 +0700

    First commit

---
+++ src/main/java/id/danielniko/beberes/BeberesApplication.java
@@ -0,0 +1,13 @@
+package id.danielniko.beberes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeberesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeberesApplication.class, args);
+	}
+
+}
